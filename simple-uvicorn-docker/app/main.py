bcddfced380b407fe084c6cf7f33a378ed115699
nestef23 <krzysztof.caderek@gmail.com>
2025-06-15 11:36:01 +0200

    Create main.py

---
+++ simple-uvicorn-docker/app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/do-something")
+async def do_something():
+    print("✅ Button clicked!")
+    return {"message": "Button clicked!"}
